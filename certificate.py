903cd3e327f1836e5738b331aceaf62a43685b07
Rohan Karan <rohankaran001@gmail.com>
2022-01-05 19:34:09 +0530

    d

--- certificate.py
+++ certificate.py
@@ -1,25 +1,32 @@
-import urllib.request
+from io import BytesIO
+import time
 import cv2
-import io
 import numpy as np
-from pandas import read_excel
+from requests import get
+from base64 import b64encode
 
 
-def generate_certificates(file) -> list:
+def generate_certificate(org, name):
     font = cv2.FONT_HERSHEY_COMPLEX
-    url = "https://www.dropbox.com/s/9rdaosfkz5baqq3/ensafe_template.png?raw=1"
-    url_response = urllib.request.urlopen(url)
-    img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
-    name_list = read_excel(file, index_col=None, na_values=['NA'], usecols="A", header=None)
-    certificates = []
+    url = "https://i.imgur.com/wf08YgQ.png"
 
-    for name in name_list[0]:
-        textsize = cv2.getTextSize(name, font, 1, 2)[0]
-        template = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        textX = (template.shape[1] - textsize[0]) // 2
-        textY = (template.shape[0] + textsize[1]) // 2 + 50
-        cv2.putText(template, name, (textX, textY), font, 1, (0, 0, 0), 2)
-        res, template = cv2.imencode(".png", template)
-        template = io.BytesIO(template.tobytes())
-        certificates.append(template)
-    return certificates
+    start = time.time()
+    img_stream = BytesIO(get(url).content)
+    end = time.time()
+    print(f"{end - start}")
+    template = cv2.imdecode(np.frombuffer(img_stream.read(), np.uint8), 1)
+
+    # adding name
+    textsize = cv2.getTextSize(name, font, 2, 3)[0]
+    textX = (template.shape[1] - textsize[0]) // 2
+    textY = (template.shape[0] + textsize[1]) // 2 + 40
+    cv2.putText(template, name, (textX, textY), font, 2, (0, 0, 0), 3)
+
+    orgsize = cv2.getTextSize(org, font, 2, 4)[0]
+    orgX = (template.shape[1]) - 700
+    orgY = (template.shape[0]) - 300
+    cv2.putText(template, org, (orgX, orgY), font, 2, (0, 0, 0), 4)
+
+    res, template = cv2.imencode(".png", template)
+    certificate = b64encode(template)
+    return {"file": certificate, "filename": f"{name}.png", "filetype": "images/png"}
--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI, File, UploadFile
-from certificate import generate_certificates
-from starlette.responses import StreamingResponse
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+from certificate import generate_certificate
+from ensafe_certificate import generate_certificates
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Name(BaseModel):
+    name: str
+    org: str
 
 
 @app.get("/")
@@ -17,8 +36,10 @@ async def say_hello(name: str):
 
 # @app.get("/cat")
 # async def cat():
-#     response = generate_certificates()
-#     return StreamingResponse(response[1], media_type="image/png")
+#     response = generate_certificate("Rohan")
+#     res = base64.b64encode(response)
+#     array = [res, res, res]
+#     return res
 
 
 @app.post("/upload/")
@@ -27,3 +48,9 @@ async def upload_excel_file(file: UploadFile = File(..., media_type="application
     file = await file.read()
     response = generate_certificates(file)
     return StreamingResponse(response[99], media_type="image/png")
+
+
+@app.post("/add/")
+async def add_name(inp: Name):
+    response = generate_certificate(inp.org, inp.name)
+    return response
