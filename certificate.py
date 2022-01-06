e4dc689e835602487caa8c5a0850d229d1c89c55
Rohan Karan <rohankaran001@gmail.com>
2022-01-06 16:25:42 +0530

    tem1

--- certificate.py
+++ certificate.py
@@ -6,7 +6,7 @@ from base64 import b64encode
 
 def generate_certificate(org, name):
     font1 = ImageFont.truetype("Cardo-Bold.ttf", 100)
-    font2 = ImageFont.truetype("Lato-Regular.ttf", 50)
+    font2 = ImageFont.truetype("Lato-Bold.ttf", 50)
     url = "https://i.imgur.com/2Uy7nTw.png"
     response = get(url)
     img = Image.open(BytesIO(response.content))
@@ -15,7 +15,7 @@ def generate_certificate(org, name):
     w1, h1 = draw.textsize(name)
     w2, h2 = draw.textsize(org)
     draw.text(((W - w1) / 2 + 40, (H - h1) / 2), name, fill="black", font=font1, anchor="ms")
-    draw.text((540, 1160), org, fill="black", font=font2, anchor="ms", stroke_width=2)
+    draw.text((540, 1160), org, fill="black", font=font2, anchor="ms")
 
     buffered = BytesIO()
     img.save(buffered, format="PNG")
