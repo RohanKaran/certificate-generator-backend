9b178af5e72bc114ecc5006e52ebfd9441e950db
Rohan Karan <rohankaran001@gmail.com>
2022-01-06 15:41:16 +0530

    tem1

--- certificate.py
+++ certificate.py
@@ -11,13 +11,12 @@ def generate_certificate(org, name):
     img = Image.open(BytesIO(response.content))
     W, H = [2000, 1414]
     draw = ImageDraw.Draw(img)
-    w, h = draw.textsize(name)
-    draw.text(((W - w) / 2 + 10, (H - h) / 2), name, fill="black", font=font1, anchor="ms")
+    w1, h1 = draw.textsize(name)
+    w2, h2 = draw.textsize(name)
+    draw.text(((W - w1) / 2 + 50, (H - h1) / 2), name, fill="black", font=font1, anchor="ms")
+    draw.text(((W - w2) / 2 - 500, (H - h2) / 2 + 300), org, fill="black", font=font1, anchor="ms")
 
     buffered = BytesIO()
     img.save(buffered, format="PNG")
     certificate = b64encode(buffered.getvalue())
     return {"file": certificate, "filename": f"{name}.png", "filetype": "images/png"}
-
-
-generate_certificate("org", "name")
