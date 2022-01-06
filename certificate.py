be1bb268eaac2c7707d81af41a263431253c6e7c
Rohan Karan <rohankaran001@gmail.com>
2022-01-06 15:44:58 +0530

    tem1

--- certificate.py
+++ certificate.py
@@ -14,7 +14,7 @@ def generate_certificate(org, name):
     w1, h1 = draw.textsize(name)
     w2, h2 = draw.textsize(name)
     draw.text(((W - w1) / 2 + 50, (H - h1) / 2), name, fill="black", font=font1, anchor="ms")
-    draw.text(((W - w2) / 2 - 500, (H - h2) / 2 + 300), org, fill="black", font=font1, anchor="ms")
+    draw.text((500, 1120), org, fill="black", font=font1, anchor="ms")
 
     buffered = BytesIO()
     img.save(buffered, format="PNG")
