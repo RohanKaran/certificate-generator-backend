82e8d75cf699db3a00e29035a00733d971d9dc59
Rohan Karan <rohankaran001@gmail.com>
2022-01-07 23:25:11 +0530

    exception handling logoimageread

--- certificate.py
+++ certificate.py
@@ -40,9 +40,8 @@ def generate_certificate(index, org, logo, name):
     font1 = ImageFont.truetype("Cardo-Bold.ttf", 100)
     font2 = ImageFont.truetype("Lato-Black.ttf", 60)
     img = Image.open(index)
-    W, H = [2000, 1414]
+    # W, H = [2000, 1414]
     draw = ImageDraw.Draw(img)
-    w1, h1 = draw.textsize(name)
     draw.text(div[index]["namePos"], name, fill=div[index]["nameColor"], font=font1, anchor="ms")
     draw.text(div[index]["oPos"], org, fill=div[index]["orgColor"], font=font2, anchor="ms")
 
